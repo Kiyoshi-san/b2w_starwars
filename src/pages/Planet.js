9080ab387f40233f122833ef9c2ab96e3cea9bbd
kiyo_yodo <kiyoshibudokan@hotmail.com>
2019-05-15 02:41:52 -0300

    Implementação Axios; Ajuste das variaveis do Redux ./pages/index.js

--- src/pages/Planet.js
+++ src/pages/Planet.js
@@ -1,7 +1,12 @@
 // import React from 'react';
 import React from 'react';
-// import '../Planet.scss';
-
+import '../style/Planet.scss';
+/**
+ LIB USADA
+ https://freefrontend.com/css-tables/
+ https://codepen.io/FlorinPop17/pen/WmNoJx
+ 
+ */
 /**
 
 planetProps:{
@@ -29,7 +34,35 @@ function Planet(res) {
     console.log("a")
     return (
         <div className="App">
-            <h1>{res.planetProps.name}</h1>
+
+            <div className="pricing-box-container">
+
+                <div className="pricing-box pricing-box-bg-image text-center">
+                    <h1 className="text-center">{res.planetProps.name}</h1>
+                    
+                    <ul className="features-list">
+                        <li><strong>Population: </strong> {res.planetProps.population}</li>
+                        <li><strong>Climate: </strong>{res.planetProps.climate}</li>
+                        <li><strong>Terrain: </strong> {res.planetProps.terrain}</li>
+                        <li><strong>Featured in </strong> {res.planetProps.films.lenght?res.planetProps.films.lenght:0} films</li>
+                    </ul>
+                    <button className="btn-primary">Next</button>
+                </div>
+
+            </div>
+
+            {/* <footer>
+                <p>
+                    Created with <i className="fa fa-heart"></i> by
+                    <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
+                    - Read about how I created it
+                    <a target="_blank" href="https://www.florin-pop.com/blog/2019/02/how-to-create-pricing-plans/">in this article</a><br />
+                    Other projects on
+                    <a target="_blank" href="https://github.com/florinpop17/work-journal/">Github</a>
+                </p>
+            </footer> */}
+
+
         </div>
     );
 }
